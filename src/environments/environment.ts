08ac95c29829b3757677d4e2ba4aa857e27aa936
Roman Yablonski <roman.yablonski@mail.ru>
2019-05-04 12:24:45 +0300

    added firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: 'AIzaSyAdDx5ENsy8n1e32EQW__HG6oK-G_6wgTA',
+      authDomain: 'wordexpert-1.firebaseapp.com',
+      databaseURL: 'https://wordexpert-1.firebaseio.com',
+      projectId: 'wordexpert-1',
+      storageBucket: 'wordexpert-1.appspot.com',
+      messagingSenderId: '55201365314',
+      appId: '1:55201365314:web:b8ec34264862d150'
+    }
 };
